cdbbca742a42358e53b7f44ab8cb505d577f383d
videv93 <videv93@gmail.com>
2024-12-22 14:14:48 +0700

    feat: update supabase anon key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://nayxvldmtjxwuohlvxhs.supabase.co';
-const supabaseAnonKey = 'your-anon-key';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5heXh2bGRtdGp4d3VvaGx2eGhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ4NDgxNjUsImV4cCI6MjA1MDQyNDE2NX0.RWKhoww6fH9t-mCEOHGKNDijCIUubyt3gYtwIMCNTAg';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
