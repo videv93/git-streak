52c148a2a9cf062d21f0bfbb45a039c647229643
MACP39 <macp39@MacBook-Pro-cua-MACP39.local>
2024-12-22 14:09:51 +0700

    feat: change supabase client id

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project-url.supabase.co';
+const supabaseUrl = 'https://nayxvldmtjxwuohlvxhs.supabase.co';
 const supabaseAnonKey = 'your-anon-key';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
